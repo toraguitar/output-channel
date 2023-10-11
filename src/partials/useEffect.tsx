00002294e89aed7d1e338438abd1bd2cae156aec
工藤 景虎 <kagetora_kudo@waku-2.com>
2023-10-11 12:02:29 +0900

    useEffectの第二引数について実践

--- src/partials/useEffect.tsx
+++ src/partials/useEffect.tsx
@@ -2,9 +2,11 @@ import React, { useEffect, useState } from "react";
 
 const UseEffect = () => {
   const [count, setCount] = useState(0);
+  const [name, setName] = useState({ lastName: '山田', firstName: '太郎' });
   useEffect(() => {
-    document.title = `${count}回クリックされました。`
-  });
+    document.title = `${count}回クリックされました。`;
+    console.log(`再レンダーされました`);
+  }, [count]);
 
   return (
     <>
@@ -16,6 +18,11 @@ const UseEffect = () => {
         <button onClick={() => setCount(0)}>
           リセット
         </button>
+        <p>{`私の名前は${name.lastName} ${name.firstName}です`}</p>
+        <form noValidate autoComplete="off">
+          <input placeholder="姓" value={name.lastName} onChange={(e) => { setName({ ...name, lastName: e.target.value }) }}></input>
+          <input placeholder="名" value={name.firstName} onChange={(e) => { setName({ ...name, firstName: e.target.value }) }}></input>
+        </form>
       </div>
     </>
   )
